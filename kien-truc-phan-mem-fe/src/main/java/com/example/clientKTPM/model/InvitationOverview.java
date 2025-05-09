d985b51e552fdf4fd2ba5e2bfe1c3f6315a4ab5b
Ngoc Yen <dieutuyetnguyenthi@gmail.com>
2025-05-09 07:59:53 +0700

    button so big

--- kien-truc-phan-mem-fe/src/main/java/com/example/clientKTPM/model/InvitationOverview.java
+++ kien-truc-phan-mem-fe/src/main/java/com/example/clientKTPM/model/InvitationOverview.java
@@ -1,5 +1,6 @@
 package com.example.clientKTPM.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,7 @@ public class InvitationOverview {
     private Long id;
     private  Tournament tournament;
     private User participant;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
     private Date createdAt;
 }
